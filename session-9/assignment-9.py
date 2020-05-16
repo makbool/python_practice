9cc1c870a4fc991ca5e69d63c78f091422d9dcdb
makbool <hussainp5@gmail.com>
2020-05-16 21:37:56 +0530

    FizzBuzz program and debugging a program

---
+++ session-9/assignment-9.py
@@ -0,0 +1,21 @@
+# Write a program to find an integer from a list of integers.
+# if found return the index of the value found else return -1
+# don't use any predefined functions
+
+
+# [66, 22, 88, 33, 67, 99, 4, 3]
+
+# input
+# 66
+# Output
+# 0
+
+# input
+# 67
+# Output
+# 4
+
+# input
+# 999
+# Output
+# -1
